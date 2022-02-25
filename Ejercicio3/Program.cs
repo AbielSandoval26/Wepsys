4d10cedc12ff877a1698779f677a2bc6f58b6c09
Abiel Sandoval <94811711+AbielSandoval26@users.noreply.github.com>
2022-02-25 12:17:54 -0400

    Update Program.cs

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -6,7 +6,26 @@ namespace Ejercicio3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x1, x2, y1, y2;
+
+            x1 = 3;
+            x2 = 4;
+            y1 = 2; 
+            y2 = 5;
+
+            var distancia = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            var suma = Math.Sqrt((Math.Pow(x1 + x2,2) + Math.Pow(y1+y2,2)));
+            var multiplicacion = Math.Sqrt((Math.Pow(x1 * x2, 2) + Math.Pow(y1 * y2, 2)));
+
+            Console.WriteLine(distancia);
+            Console.WriteLine(suma);
+            Console.WriteLine(multiplicacion);
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
