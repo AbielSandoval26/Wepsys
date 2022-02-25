7bba1e8f2d472e22f36d7c106d96cdc615e8c35b
Abiel Sandoval <20197806@itla.edu.do>
2022-02-25 11:24:30 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wepsys
+{
+    class Program
+    {
+        static void PosiblesAmigos()
+        {
+            string Nombre = "Abiel";
+            //string[] Amigos = new string[] { "Juan", "Javier", "Juana", "Marcos" };
+
+            List<string> amigos = new List<string>()
+                { "Juan", "Javier", "Juana", "Marcos" };
+
+            /*IEnumerable<string> enumerable()
+            {
+                return from string amigo in amigos
+                       where amigo.Length == Nombre.Length
+                       select amigo;
+                Console.WriteLine();
+
+            };*/
+
+            /*foreach (var amigo in amigos)
+            {
+                if (amigo == 5)
+                {
+                    Console.WriteLine(amigo);
+                };
+            }*/
+
+            
+
+        }
+
+        static void Main(string[] args)
+        {
+            PosiblesAmigos();
+            Console.ReadKey();
+            
+            
+        }
+
+        
+
+    }
+}
