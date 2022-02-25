8e4b8a06976efdb65ed0eaa9dfb0397825788582
Abiel Sandoval <20197806@itla.edu.do>
2022-02-25 11:29:02 -0400

    ...

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Oracion = "Maria lava la ropa";
+
+            char delimitador = ' ';
+            string[] valores = Oracion.Split(delimitador);
+
+            for (int y = valores.GetLength(0) -1; y>=0; y-- )
+            {
+                Console.WriteLine(valores[y] + " " + valores[y].Length);
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
