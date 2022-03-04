8cf120284fcd37921e5f66f0edb49230f3a278ec
Abiel Sandoval <94811711+AbielSandoval26@users.noreply.github.com>
2022-03-03 20:58:59 -0400

    Update Program.cs

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -8,15 +8,19 @@ namespace Wepsys
     {
         static void PosiblesAmigos()
         {
+            //NOTA: ESTE CODIGO FUE EL REALIZADO DURANTE LA PRUEBA EN VIVO!!!
+            
             string Nombre = "Abiel";
             //string[] Amigos = new string[] { "Juan", "Javier", "Juana", "Marcos" };
 
             List<string> amigos = new List<string>()
                 { "Juan", "Javier", "Juana", "Marcos" };
 
-            char delimitador = ',';
-
-            string[] PA = amigos.split
+            /* Esto no es utilizado pero aun asi lo dejo para que quede como demostrable
+            de que no realice ningun cambio bruzcamente.*/
+            
+            char delimitador = ',';  
+            string[] PA = amigos.split 
 
             /*IEnumerable<string> enumerable()
             {
@@ -25,8 +29,17 @@ namespace Wepsys
                        select amigo;
                 
             };*/
+                
+            //NOTA: ESTE foreach fue agregado despues
 
-            Console.WriteLine();
+            foreach (string amigo in amigos)
+            {
+                if (amigo.Length == Nombre.Length) 
+                {
+                    Console.WriteLine("Tu posible amigo es " + amigo);
+                }
+                
+            }
 
             /*foreach (var amigo in amigos)
             {
